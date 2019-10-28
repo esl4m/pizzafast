2072e21db76d12c5a5c5ebc6ff884b06fff92a2a
Eslam Elgamal <me@esl4m.com>
2019-10-29 02:55:39 +0300

    Adds test case for pizza

--- pizza_app/tests.py
+++ pizza_app/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import Pizza
 
-# Create your tests here.
+
+class PizzaTest(TestCase):
+    """ Test module for Pizza model """
+
+    def setUp(self):
+        self.pizza_url = '/api/pizza/'
+        self.client = Client()
+
+        self.name = 'Margarita'
+
+        Pizza.objects.create(
+            name='Margarita', small_size_price=10, med_size_price=15)
+
+
+    def test_get_all_pizza(self):
+        response = self.client.get(self.pizza_url)
+        self.assertEqual(response.status_code, 200)  # response 200 .
